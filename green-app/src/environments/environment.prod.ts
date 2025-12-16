0dc72d322c8b8f2492556d73027231b17b56132b
hieuoneplus <nshd183@gmail.com>
2025-12-17 00:03:37 +0700

    update backend url

--- green-app/src/environments/environment.prod.ts
+++ green-app/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  apiBase: 'https://your-backend-url.com', // Thay bằng URL backend production của bạn
+  apiBase: 'https://environment-app-m9ir.onrender.com', // Thay bằng URL backend production của bạn
   production: true
 };
